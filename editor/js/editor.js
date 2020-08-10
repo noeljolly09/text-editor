447205b443c53e02e84f9885c496602dd3036ddc
noel <noel.jmail09@gmail.com>
2020-08-10 16:48:26 +0530

    some changes

--- editor/js/editor.js
+++ editor/js/editor.js
@@ -14,3 +14,23 @@ function execCmd(command){
   //   var x = document.getElementById("textfield");
   //   x.style.fontcolor = "white";
   // }
+
+  // function createTable(){
+  //   var table = '';
+  //   var rows;
+  //   var cols;
+  //
+  //   rows = prompt("enter no. of rows");
+  //   cols = prompt("enter no. of cols");
+  //
+  //   for(r = 1; r<= rows; r++)
+  //   {
+  //     table += '<tr>';
+  //     for(var t = 1; t<= cols; t++)
+  //     {
+  //       table += '<td>' + t +'</td>';
+  //     }
+  //     table += '</tr>';
+  //   }
+  //   document.write('<table border=1>' + table + '</table>');
+  // }
